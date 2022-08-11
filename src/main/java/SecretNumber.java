1c4a72dbe4168351b47a7b9c32a567c6498d7106
Kai Chan <owenkaic@gmail.com>
2022-08-11 19:19:29 +0100

    added user prompt and storage of user guess

--- src/main/java/SecretNumber.java
+++ src/main/java/SecretNumber.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
+
 public class SecretNumber {
 
     public static void main(String[] args) {
 
-//  Put code in here
+//      Put code in here
+//      set the secret number
         int secretNumber = 10;
+
+
+//        prompt user to guess a number
+
+        System.out.println("Take a guess at the secret number and enter an integer");
+
+//        collect user input
+
+        Scanner reader = new Scanner(System.in); // create scanner object
+        String guessNumber = reader.nextLine();
+
+
+
+//      convert the stored value of guessNumber into an int not String
+//        check to see if the input number is equal to secret number
+//        if number guessed is equal to secret number, tell user the guess is correct
+//        if the number guessed is not equal to secret number, tell user whether it is bigger or smaller
+
+//      Quick test code
+        System.out.println(guessNumber);
+        System.out.println(secretNumber);
     }
+
 }
